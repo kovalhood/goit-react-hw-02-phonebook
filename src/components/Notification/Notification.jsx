71f630ec1fac0acd90bf4605d0032266fecb8074
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-05-13 12:48:16 +0300

    Class-Correction

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,7 +2,7 @@ import s from './Notification.module.css';
 
 const Notification = ({message}) => {
     return <p className={s.notification}>
-        <i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;
+        <i className="fa fa-info-circle" aria-hidden="true"></i>&nbsp;
         {message}
     </p>
 }
